8455cbf1f40d93bc8215b5029191c99e613cc85e
Nemeth Gergely <neged.ng@gmail.com>
2017-03-20 08:50:07 -0700

    JUnit test for TrainSensorImpl

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -3,17 +3,47 @@ package hu.bme.mit.train.sensor;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.junit.MockitoJUnitRunner.StrictStubs;
+
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainUser;
+import hu.bme.mit.train.user.TrainUserImpl;
+
 import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
-
+	
+	TrainSensorImpl ts;
+	TrainUserImpl mockTUI;
+	TrainController mockTC;
+	
     @Before
-    public void before() {
-        // TODO Add initializations
+    public void init() {
+        mockTUI = mock(TrainUserImpl.class);
+        mockTC = mock(TrainController.class);
+         ts= new TrainSensorImpl(mockTC,mockTUI);
     }
 
     @Test
-    public void ThisIsAnExampleTestStub() {
-        // TODO Delete this and add test cases based on the issues
+    public void overrideSpeedLimit_smallerThanMin_GetAlarm() {
+        ts.overrideSpeedLimit(-1);
+        verify(mockTUI, times(1)).setAlarmState(true);
+    }
+    @Test
+    public void overrideSpeedLimit_greaterThanMax_GetAlarm() {
+        ts.overrideSpeedLimit(501);
+        verify(mockTUI, times(1)).setAlarmState(true);
+    }
+    @Test
+    public void overrideSpeedLimit_tooBigChange_GetAlarm() {
+    	when(mockTC.getReferenceSpeed()).thenReturn(150);
+        ts.overrideSpeedLimit(50);
+        verify(mockTUI, times(1)).setAlarmState(true);
+    }
+    @Test
+    public void overrideSpeedLimit_correctChange_NoAlarm() {
+    	when(mockTC.getReferenceSpeed()).thenReturn(100);
+        ts.overrideSpeedLimit(120);
+        verify(mockTUI, times(1)).setAlarmState(false);
     }
 }
